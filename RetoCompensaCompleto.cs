b89a6ee97573e9c35689271c5065b5d046a9fa59
Metz001 <46971952+Metz001@users.noreply.github.com>
2019-01-30 10:10:52 -0500

    Add files via upload

---
+++ RetoCompensaCompleto.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetoCompensaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deduciones slariales. Este programa te dirá cuamto ganas realmente al restar las deduciones salariales");
+            Console.WriteLine("Ingrese su salario actual");
+            double smmlv = 828116;
+            double salario = double.Parse(Console.ReadLine());
+            double cotización = salario * 40 / 100;
+            if (salario < smmlv)
+            {
+                cotización = smmlv * 40 / 100;
+            }
+            Console.WriteLine("¿caul es su tipo de contrato?, Tipo de contrato: 1. dependiente | 2. independiente");
+            Console.WriteLine("ingrese 1 para depndiente, y 2 para contrato independiente");
+            double contrato = double.Parse(Console.ReadLine());
+
+
+
+
+            //Si y sólo si el tipo de contrato es independiente se pide que se ingrese un número de 1 a 5 equivalente a la clase del riesgo.
+            if (contrato == 1 || contrato == 2)
+            {
+                        if (contrato == 1)
+                        {
+                            double pension = 4 * cotización / 100;
+                            double eps = 4 * cotización / 100;
+                            double arl = 0;
+                            double prima = salario;
+
+                            Console.WriteLine("Las dedudciones que recibirá son: \n");
+
+                            Console.WriteLine("Pension {0}", pension * -1);
+                            Console.WriteLine("Eps {0}", eps * -1);
+                            Console.WriteLine("Arl {0}", arl);
+                            Console.Write("total deduciones = {0}", pension + eps + arl);
+                            Console.WriteLine("su salario real corresponde a {0}", salario - pension - eps - arl);
+                            Console.WriteLine("su salario anuel sin deducciones más prima corresponde a {0}", salario * 12 + salario);
+
+                        }
+
+                        if (contrato == 2)
+                        {
+                    Console.WriteLine("Seleccione el riesgo de 1 a 5 de la actividad que va a desempeñar basado en la lista que se presenta a continuación: ");
+              
+                    Console.WriteLine("1. Financieras, trabajos de oficina, administrativos, centros educativos, restaurantes");
+                    Console.WriteLine("2. Algunos procesos manufactureros como fabricación de tapetes, tejidos, confecciones y flores artificiales, almacén por departamentos, algunas labores agrícolas.");
+                    Console.WriteLine("3. Algunos procesos manufactureros como la fabricación de agujas, alcoholes y artículos de cuero");
+                    Console.WriteLine("4. Procesos manufactureros como fabricación de aceites, cervezas, vidrios, procesos de galvanización, transportes y servicios de vigilancia privada.");
+                    Console.WriteLine("5. Areneras, manejo de asbesto, bomberos, manejo de explosivos, construcción y explotación petrolera.");
+                    double riesgo = double.Parse(Console.ReadLine());
+
+
+                    if (riesgo == 1)
+                        riesgo = 0.522;
+
+                    if (riesgo == 2)
+                        riesgo = 1.044;
+
+                    if (riesgo == 3)
+                        riesgo = 2.436;
+
+                    if (riesgo == 4)
+                        riesgo = 4.350;
+
+                    if (riesgo ==5)
+                        riesgo = 6.960;
+
+                    double pension = 16 * cotización / 100;
+                     double eps = 12.5 * cotización / 100;
+                    double arl = riesgo * cotización / 100;
+
+                    Console.WriteLine("Pension {0}", pension * -1);
+                    Console.WriteLine("Eps {0}", eps * -1);
+                    Console.WriteLine("Arl {0}", arl);
+                    Console.Write("total deduciones = {0}", pension + eps + arl);
+                    Console.WriteLine("su salario real corresponde a {0}", salario - pension - eps - arl);
+                    Console.WriteLine("su salario anuel sin deducciones más prima corresponde a {0}", salario * 12 + salario);
+
+
+
+                }
+                       
+                    
+
+
+
+                  
+                
+                    
+                    
+                   
+              
+
+
+
+            }
+
+            
+
+
+
+        }
+    }
+}
---
+++ RetoVotraciones2Resuelto.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    /*
+     * Si el total de votos es superior a la población, O la diferencia de votos entre el partido 1
+     * y el partido 2 es menor al 10% de los votos totales Y el Número de votantes es inferior al 30% de la población
+     * 
+     * a: número de votos por el partido 1
+
+        b: número de votos por el partido 2
+
+        blancos: número de votos en blanco
+
+        anulados: número de votos anulados
+
+        n: número total de la población de todas las edades
+
+        p: el porcentaje (de 0 a 100%) de la población que es mayor de edad
+
+
+           while (esCorrecto == false)
+            {
+                try
+                {
+                    Console.Write("Ingresa un numero entero positivo: ");
+                    numero = long.Parse(Console.ReadLine());
+
+                    //Validamos que sea entero positivo hasta 4 cifras
+                    if (numero > 0 )
+                        esCorrecto = true;
+                    else
+                        Console.WriteLine("El dato ingresado no es un entero positivo. Intenta nuevamente! \n");
+
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para la comprovación de datos de las eleciones");
+
+
+            bool RepetirVotaciones = true;
+
+            while (RepetirVotaciones == true)
+            {
+                try
+                {
+                    Console.WriteLine("Ingrese la población total de habitantes");
+                    double n = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese el porcentaje de la población que puede votar");
+                    double p = double.Parse(Console.ReadLine());
+
+                    double Votantes = p * n / 100;
+                    if (p > 100)
+                        p = 100;
+
+                    Console.WriteLine("ingrese los votos del partido 1");
+                    double Partido1 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("ingrese los votos del partido 2");
+                    double Partido2 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("ingrese los votos en blanco");
+                    double Blanco = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("ingrese los votos nulos");
+                    double Nulos = double.Parse(Console.ReadLine());
+
+                    double DifPartidos = Partido1 - Partido2;
+                    if (DifPartidos < 0)
+                        DifPartidos = DifPartidos * -1;
+
+                    double totalVotos = Partido1 + Partido2 + Blanco;
+
+                    double DiezPercentVotos = totalVotos * 10 / 100;
+
+                    double treintaPoblacion = n * 30 / 100;
+
+                    if (totalVotos > Votantes || DifPartidos <DiezPercentVotos && Votantes < treintaPoblacion )
+                    {
+
+                        Console.WriteLine("Se ha presentado el siguiente problema en las eleciones \n");
+                        if (totalVotos > Votantes)
+                        {
+                            Console.WriteLine("el total de votos es mayor al númeor de votantes. Repetir eleciones o hacer un reconteo \n");
+                        }
+
+                        if (DifPartidos < DiezPercentVotos && Votantes < treintaPoblacion)
+
+                            Console.WriteLine("La diferencia de votos entre el partido 1 y el partido 2 es menor al 10% de los votos totales Y el Número de votantes es inferior al 30% de la población");
+                        RepetirVotaciones = true;
+                     
+                    }
+
+                  
+
+                    else
+                    {
+
+                        Console.WriteLine("Las eleciones han sido un éxito");
+                        if (Partido1 > Partido2)
+                        {
+                            Console.WriteLine("El partido1 es el victorioso \n");
+                        }
+
+
+                        Console.WriteLine("Las eleciones han sido un éxito");
+                        if (Partido2 > Partido1)
+                        {
+                            Console.WriteLine("El partido1 es el victorioso \n");
+                        }
+                        RepetirVotaciones = false;
+                    }
+                }
+
+                catch (FormatException error)
+                {
+                    Console.WriteLine("Ingresaste un dato no numérico. Intenta nuevamente!");
+                    Console.WriteLine("Error: " + error.Message + "\n");
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
