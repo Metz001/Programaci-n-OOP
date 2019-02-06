b35510e0cc9766879ca6d14f3e8bd9a9dfc7864a
Metz001 <46971952+Metz001@users.noreply.github.com>
2019-02-06 08:51:37 -0500

    Add files via upload

---
+++ DadosNoR.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados1y1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int D1 = random.Next(1, 11);
+            int D2 = random.Next(1, 11);
+            int puntos = D1 + D2;
+
+            if (D1 == 1 && D2 == 1)
+            {
+                Console.WriteLine("sacaste 1 en ambos dados, perdiste");
+                puntos = -1;
+            }
+
+            while (puntos < 100 && puntos > 0)
+            {
+                Console.WriteLine("Por ahora tines {0} puntos, quieres continuar? s para sí, n para No", puntos);
+                int Jugó = 0;
+                string respuesta = (Console.ReadLine()
+
+                if (Console.ReadLine() == "s") Jugó++;
+                if (Console.ReadLine() == "n") break;
+                D1 = random.Next(1, 11);
+                D2 = random.Next(1, 11);
+                int More6 = 0;
+                
+                if (D1 + D2 > 6) More6++;
+                puntos += D1 + D2;
+               
+                
+               
+                if (D1 ==1 && D2 == 1)
+                    {
+                        Console.WriteLine("sacaste 1 en ambos dados, perdiste");
+                    break;
+                   
+                    }
+                int porcentaje = More6 / Jugó * 100;
+                Console.WriteLine("El porcentaje de la veces que sacó más de 6 fue {0}", porcentaje);
+            }
+            if (puntos > 100) Console.WriteLine("Bien Hecho, has sacado la puntuación máxima, (100)");
+               
+
+
+        }
+    }
+}
---
+++ ReValidación.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicoValidación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido, es usted humano?, realice la sigueinte operación. Solo tiene 5 intentos");
+            Random random = new Random();
+            int N1 = random.Next(1, 51);
+            int N2 = random.Next(1, 51);
+            int intentos = 0;
+
+            while (intentos < 3)
+            {
+                Console.WriteLine("cuanto es {0} + {1} ", N1, N2);
+                int SuRespuesta = int.Parse(Console.ReadLine());
+                int suma = N1 + N2;
+
+
+
+                if (SuRespuesta != suma)
+                {
+                    Console.WriteLine("La suma no es {0}", SuRespuesta);
+                    intentos++;
+                }
+                if (SuRespuesta == suma)
+                {
+                    Console.WriteLine("Bien Hecho, usted es humano, continue");
+                    break;
+                }
+
+
+            }
+            if (intentos == 3) Console.WriteLine("Intentos Superoados. se cerrará el progrma");
+        }
+    }
+}
