6f5cc78eacd5bc7ffde69558f287e9f172114230
Metz001 <46971952+Metz001@users.noreply.github.com>
2019-02-27 13:41:35 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Program {
+
+    public static void Main (string[] args) {
+
+        Vector3 pos1 = new Vector3(0, 0, 0);
+        Carro Carro1 = new Carro(pos1);
+
+        Carro1.pintarTodo();
+        Carro1.abretodo();
+    }
+
+
+}
